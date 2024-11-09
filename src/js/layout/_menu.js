d1fe7a3aa1abd6b8821ad38ed91e03a6b6356236
Tyler Brock <pedrohardbass@gmail.com>
2024-11-09 00:59:37 -0700

    v0.0.0.0.0.0.3 ejemplo de boton

--- src/js/layout/_menu.js
+++ src/js/layout/_menu.js
@@ -3,9 +3,8 @@ function mostrar_ordenes() {
     const menu = document.getElementById("opciones")
     // menu.insertBefore(,menu);
     const div = document.createElement("DIV");
-    div.className = "opcion";
+    div.className = "menu-nav contenedor";
     const section = document.createElement("SECTION");
-    section.className = "menu-imagen contenedor";
-    const aside = document.createElement("ASIDE");
-    aside.className = "menu-descripcion";
+    section.className = "";
+
 }
